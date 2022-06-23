8a3b4998bbed9ee0c1d4f6543d2eee05bd1117c1
DaisyChepyegon <daisychepyegon@gmail.com>
2022-06-23 15:14:28 +0300

    style register form

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
   .catch(error =>  console.log(error))
 
 }
+const cList = document.querySelector("#cocktailList");
+let Name = document.getElementById("beer-name");
+let Image = document.getElementById("beer-image");
+let beerDescription = document.getElementById("beer-description");
+let beerReviews =document.getElementById("review-list")
+function listCocktails(drinks) {
+  drinks.forEach((drinks) => {
+    const list = document.createElement("li");
+    list.innerText = drinks.strDrink;
+
+    cList.appendChild(list);
+    list.addEventListener("click", () => {
+      beerName.textContent = drinks.strDrink;
+      beerImage.setAttribute("src", data.image_url);
+      beerDescription.textContent = data.description;
+      beerReviews.textContent = data.reviews;
+    });
+  });
+}
