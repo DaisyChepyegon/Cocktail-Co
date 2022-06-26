483bfbbba8977c1aa3680fc0e1bcca0739519748
DaisyChepyegon <daisychepyegon@gmail.com>
2022-06-26 21:18:43 +0300

    add users database

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -6,8 +6,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+
+
+//display first drink
+// fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=17222')
+// .then(res => res.json())
+// .then (data =>
+//   {
+     
+//      const image = document.querySelector("#img").setAttribute("src", data.strDrinkThumb);
+     
+//      const instruction = document.querySelector("#instructions").innerText = data.strInstructions;
+
+// })
+
+
+
+
 function getData(){
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
       .then(response =>response.json())
       .then((data) => {
         //console.log(data[0])
@@ -31,7 +48,7 @@ function listCocktails(drink) {
     cocktailList.appendChild(list);
     list.addEventListener("click", () => {
 
-      image.setAttribute("src", element.strDrinkThumb);
+      image.setAttribute("src",element.strDrinkThumb)
       instruction.innerHTML = element.strInstructions;
 
     if(element.strIngredient1 !== null){
@@ -69,24 +86,30 @@ function listCocktails(drink) {
   }
   if(element.strMeasure2 !== null){
     const measure1 = document.createElement('li')
-    measure2.textContent = element.strMeasure2;
+    measure1.textContent = element.strMeasure2;
     measurement.appendChild(measure1)
   }
   if(element.strMeasure3 !== null){
     const measure2 = document.createElement('li')
-    measure2.textContent = element.strMeasure2;
+    measure2.textContent = element.strMeasure3;
     measurement.appendChild(measure2)
   }
-  if(element.strMeasure3 !== null){
+  if(element.strMeasure4 !== null){
     const measure3 = document.createElement('li')
-    measure3.textContent = element.strMeasure3
+    measure3.textContent = element.strMeasure4
     measurement.appendChild(measure3)
+  } 
+  if(element.strMeasure5 !== null){
+    const measure4 = document.createElement('li')
+    measure4.textContent = element.strMeasure5
+    measurement.appendChild(measure4)
   } 
   
 
     });
   })
 }
+
 
 
 
@@ -101,11 +124,10 @@ function listCocktails(drink) {
       <div id="drinks">
 
         <img src=${value.strDrinkThumb} alt="img" class ="images">
-        <h3>${value.strDrink}</h3>
-        <li class="like">Like <span class="like-glyph">&#x2661;</span></li>
+        <h3>${value.strDrink}</h3><br>
+        <h4 id="add"><span id="like">&#x2661;</span>  like </h4> 
 
-      </div>
-               `
+      </div>`
       });
       document.getElementById("drink").innerHTML = drinks;
     }).catch((Error) => {
@@ -113,36 +135,43 @@ function listCocktails(drink) {
     })
 
 
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+    // document.getElementById('like').addEventListener('click', changeColor);
 
- const hearts = document.querySelectorAll(".like-glyph");
- 
+    //    function changeColor() {
+    //      this.style.color = "red";
+    //      return false; 
+    //    }
 
-  const likePost = (e) =>{ 
 
-const heart = e.target
-heart.innerText = EMPTY_HEART
-mimicServerCall()
+    // document.getElementsByClassName('fa fa-heart').addEventListener('click', function onClick() {
+    //    style.color = "red";
 
-  .then( responce => {
-    (heart.innerText === EMPTY_HEART)?
-    (heart.innerText = FULL_HEART, heart.classList.add("activated-heart"))
-    :(heart.innerText = EMPTY_HEART, heart.classList.remove("activated-heart"))
-  })
-
-  
-}
-
-hearts.forEach( heart => {
-heart.addEventListener("click", likePost)
-})
+    // });
 
 
 
 
 
+// const EMPTY_HEART = '♡'
+// const FULL_HEART = '♥'
 
+// const like=document.querySelector('#like')
+// like.addEventListener('click', (e) =>{
+
+//  const heart = e.target;
+//  heart.innerHTML = EMPTY_HEART
+
+//  if(heart.innerHTML === EMPTY_HEART){
+//      heart.innerHTML == FULL_HEART;
+//      heart.classList.add("activated-heart")
+     
+//    }
+//    else {
+//      heart.innerHTML == EMPTY_HEART;
+//      heart.classList.remove("activated-heart")
+//    }
+
+//   })
 
 
 
--- assets/JS/login.js
+++ assets/JS/login.js
@@ -46,4 +46,62 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
 
+
+  document.addEventListener("DOMContentLoaded", () => {
+    fetchUsers();
+  });
+  const logInForm = document.getElementById("input-login");
+  const email = document.getElementById("l-email");
+  const password = document.getElementById("l-password");
+  const form = document.getElementById("login");
+  const adminPanel = document.getElementById("btn-admin");
+  function fetchUsers() {
+    fetch("http://localhost:3000/users")
+      .then((resp) => resp.json())
+      .then((users) => {
+        loginAuth(users);
+      });
+  }
+  //login function
+  function loginAuth(users) {
+    logInForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+  
+      let acc;
+      for (let data of users) {
+        //vadidates email
+        if (email.value == "") {
+          alert("Email required!");
+          return;
+        }
+        //validate password
+        if (password.value == "") {
+          alert("Password required!");
+          return;
+        }
+        //authenticate email
+        if (!(email.value === data.email)) {
+          alert("Not a registered username");
+          password.value = "";
+          return;
+        }
+        //authenticate password
+        acc = data;
+        if (!(password.value === acc.password)) {
+          alert("Invalid password!");
+          password.value = "";
+          return;
+        }
+        if (!(acc.usertype === "admin")) {
+          adminPanel.disabled = true;
+        }
+        alert(`welcome back ${data.name}`);
+        //console.log(acc);
+        form.style.display = "none";
+        return;
+      }
+    });
+  }
+
+
   
